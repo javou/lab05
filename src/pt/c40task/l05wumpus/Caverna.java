7553e83f6a2735717d3a3f0673b1574ddfdac9df
Artur <arturpaulos22@gmail.com>
2022-05-04 15:43:10 -0300

    desenvolve Montador.java

--- src/pt/c40task/l05wumpus/Caverna.java
+++ src/pt/c40task/l05wumpus/Caverna.java
@@ -3,5 +3,11 @@ package pt.c40task.l05wumpus;
 public class Caverna {
 	private Sala[][] salas = new Sala[4][4];
 	
+	public void incluirComponente(Componente componente) {
+		// adiciona componente na sala de mesmas coordenadas.
+	}
 	
+	public void moverComponente() {
+		
+	}
 }
